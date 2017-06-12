91069588dc843816102d064cd81f1cb6f4b1faa5
Federico Coraglio <federicocoraglio@gmail.com>
2017-06-12 09:17:39 -0300

    Ejercicio 10 - Agregar un usuario utilizando una llamada POST Http en el servicio

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs';
+import { User } from '../models/user';
 
 const SERVER_REST_API_URL = "http://localhost:3000/users/";
 
@@ -16,6 +17,17 @@ export class UserService {
   getUsers() {
     return this.http.get(SERVER_REST_API_URL)
         .map(res => res.json());
+  }
+
+  saveUser(user:User) {
+    let body = JSON.stringify({
+        "id": user.id,
+        "name": user.name,
+        "username": user.username
+    });
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.post(SERVER_REST_API_URL, body, options);
   }
 
 }
--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { User } from '../shared/models/user';
+import { UserService } from '../shared/services/user.service';
 
 @Component({
   selector: 'app-user-form',
@@ -9,12 +10,21 @@ import { User } from '../shared/models/user';
 export class UserFormComponent implements OnInit {
   @Output() userCreatedEvent = new EventEmitter();
   newUser: User;
+  userService:UserService;
+  active:boolean= true;
 
-  constructor() { }
+  constructor(userService: UserService) {
+    this.userService = userService;
+  }
 
   onSubmit() {
-    this.userCreatedEvent.emit({user: this.newUser});
-    this.newUser = new User("","","");
+    this.userService.saveUser(this.newUser)
+        .subscribe(()=>{
+          this.userCreatedEvent.emit({user: this.newUser});
+          this.active = false;
+          this.newUser = new User("","","");
+          setTimeout(() => {  this.active = true; }, 0);          
+      });        
   }
 
   ngOnInit() {
