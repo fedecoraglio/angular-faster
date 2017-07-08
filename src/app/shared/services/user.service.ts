4da3bad48abb9249c28155e8dc3247391385b475
Federico Coraglio <federicocoraglio@gmail.com>
2017-07-08 15:27:16 -0300

    Ejercicio 15 - Soportar diferentes llamadas a servicios Rest API

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
 import 'rxjs';
 import { User } from '../models/user';
+import { environment } from '../../../environments/environment'
 
 const SERVER_REST_API_URL = "http://localhost:3000/users/";
 
@@ -9,18 +10,20 @@ const SERVER_REST_API_URL = "http://localhost:3000/users/";
 export class UserService {
 
   private http: Http;
+  private serverRestAPIUrl: string;
 
   constructor(http:Http) {
     this.http = http;
+    this.serverRestAPIUrl = environment.apiEndPoint + "/users";
   }
 
   getUsers() {
-    return this.http.get(SERVER_REST_API_URL)
+    return this.http.get(this.serverRestAPIUrl)
         .map(res => res.json());
   }
 
   deleteUser(id) {
-    return this.http.delete(SERVER_REST_API_URL + id);
+    return this.http.delete(this.serverRestAPIUrl + id);
   }
 
   saveUser(user:User) {
@@ -31,7 +34,7 @@ export class UserService {
     });
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
-    return this.http.post(SERVER_REST_API_URL, body, options);
+    return this.http.post(this.serverRestAPIUrl, body, options);
   }
 
 }
